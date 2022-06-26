f3c49f16b33b3cc8174388c7390a40de000fcbe9
marieono <104805318+marieono@users.noreply.github.com>
2022-06-26 12:40:10 +0900

    React入門2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { List } from "./List";
 
 function App() {
+  const [description, setDescription] = useState('クリック前の表示');
+
+  const changeDescription = () => {
+    setDescription('クリック後の表示です。');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {description}
+      <List title="取扱言語一覧" />
+      <button onClick={changeDescription}>ボタン</button>
     </div>
+
   );
 }
 
---
+++ src/List.js
@@ -0,0 +1,9 @@
+export const List = ({ title }) => {
+    return (
+        <div>
+            <h4>{title}</h4>
+            < div > リストです</div >
+        </div>
+    )
+
+}
