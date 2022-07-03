29b72775e9aa8592cd377b25f9457ce7d2a8efe4
marieono <104805318+marieono@users.noreply.github.com>
2022-07-03 09:48:00 +0900

    変更？

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { List } from './List';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: 'クリック前の表示' }
+  }
+
+  changeDescription() {
+    this.setState({
+      description: 'クリック後の表示です。'
+    })
+  }
+
   render() {
+    const { description } = this.state;
     return (
-      <div>クラスコンポーネントにしてみました。あ</div>
+      <div>
+        {description}
+        <List title="取扱い言語一覧" />
+        <button onClick={() => this.changeDescription()}>ボタン</button>
+      </div>
     )
   }
 }
