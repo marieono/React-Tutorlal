31728f22a0974b94abe8f82045300cc3c41fd52e
marieono <104805318+marieono@users.noreply.github.com>
2022-07-10 09:05:40 +0900

    クラスコンポーネントもフォームと親子間のデータのやり取り終了

--- src/From.js
+++ src/From.js
@@ -1,10 +1,31 @@
 import React from "react";
 
 export class Form extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: '' }
+    }
+    submitForm(event) {
+        event.preventDefault();
+        this.props.onAddLang(this.state.text);
+    }
+
+
     render() {
+        const { text } = this.state;
         return (
             < div >
-                フォームです
+                <h4>新しい言語を追加</h4>
+                <form onSubmit={(e) => this.submitForm(e)}>
+                    <div>
+                        <input type="text" value={text}
+                            onChange={(e) => this.setState({ text: e.target.value })}
+                        />
+                    </div>
+                    <div>
+                        <button>追加</button>
+                    </div>
+                </form>
             </div >
         )
     }
