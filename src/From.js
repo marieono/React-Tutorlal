98a54cdd4401601d4a40b205e782df1e6c8b6517
marieono <104805318+marieono@users.noreply.github.com>
2022-07-18 14:38:05 +0900

    styledでCssを追加した

--- src/From.js
+++ src/From.js
@@ -1,4 +1,32 @@
 import { useState } from "react";
+import { Button } from "./components/button.js";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+    padding: 12px 64px;
+`
+
+const Label = styled.label`
+    display: flex;
+    color: #757575;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+const Input = styled.input`
+    border-radius: 3px;
+    padding: 4px 8px;
+    border: 1px soild black;
+`
+
+const ButtonContainer = styled.div`
+    margin-top: 24px;
+`
+
+const FormButton = styled(Button)`
+    width: 120px;
+`
 
 export const Form = ({ onAddLang }) => {
     const [text, setText] = useState('');
@@ -9,18 +37,19 @@ export const Form = ({ onAddLang }) => {
 
 
     return (
-        < div >
+        < Container >
             <h4>新しい言語の追加</h4>
             <form onSubmit={submitForm}>
                 <div>
-                    <input type="text" value={text}
+                    <Label>言語</Label>
+                    <Input type="text" value={text}
                         onChange={(e) => setText(e.target.value)}
                     />
                 </div>
-                <div>
-                    <button>追加</button>
-                </div>
+                <ButtonContainer>
+                    <FormButton>追加</FormButton>
+                </ButtonContainer>
             </form>
-        </div >
+        </Container >
     )
 }
--- src/List.js
+++ src/List.js
@@ -1,9 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    padding: 12px 64px;
+`
+
+const ListItem = styled.div`
+    padding: 8px 16px;
+
+    &:nth-child(n+2){
+        border-top: 1px solid #D9DBDE;
+    }
+`
 export const List = ({ langs }) => {
     return (
         <div>
             {
                 langs.map((lang, index) => {
-                    return <div key={index}>{lang}</div>
+                    return <ListItem key={index}>{lang}</ListItem>
                 })
             }
         </div>
