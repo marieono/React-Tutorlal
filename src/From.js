4ab6a9c1cb36400093b41205d11907c9f6688b43
marieono <104805318+marieono@users.noreply.github.com>
2022-07-18 15:11:45 +0900

    クラスコンポーネントもCSS完成した

--- src/From.js
+++ src/From.js
@@ -1,4 +1,31 @@
 import React from "react";
+import styled from 'styled-components';
+import { Button } from './components/button.js'
+
+const Container = styled.div`
+    padding: 12px 64px;
+`
+
+const Label = styled.label`
+    display: flex;
+    color: #757575;
+    font-size: 14px;
+    font-weight: bold;
+`
+
+const Input = styled.input`
+    border-radius: 3px;
+    padding: 4px 8px;
+    border: 1px solid black;
+`
+
+const ButtonContainer = styled.div`
+    margin-top: 24px;
+`
+
+const FormButton = styled(Button)`
+    width: 120px;
+`
 
 export class Form extends React.Component {
     constructor(props) {
@@ -14,19 +41,20 @@ export class Form extends React.Component {
     render() {
         const { text } = this.state;
         return (
-            < div >
+            < Container >
                 <h4>新しい言語を追加</h4>
                 <form onSubmit={(e) => this.submitForm(e)}>
                     <div>
-                        <input type="text" value={text}
+                        <Label>言語</Label>
+                        <Input type="text" value={text}
                             onChange={(e) => this.setState({ text: e.target.value })}
                         />
                     </div>
-                    <div>
-                        <button>追加</button>
-                    </div>
+                    <ButtonContainer>
+                        <FormButton>追加</FormButton>
+                    </ButtonContainer>
                 </form>
-            </div >
+            </Container >
         )
     }
 }
--- src/List.js
+++ src/List.js
@@ -1,16 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    padding: 12px 64px;
+`
+const ListItem = styled.div`
+    padding: 8px 16px;
+
+    &:nth-child(n+2){
+        border-top:1px solid #D9DBDE;
+    }
+`
 
 export class List extends React.Component {
     render() {
         const { langs } = this.props;
         return (
-            <div>
+            <Container>
                 {
                     langs.map((lang, index) => {
-                        return <div key={index}>{lang}</div>
+                        return <ListItem key={index}>{lang}</ListItem>
                     })
                 }
-            </div>
+            </Container>
         )
     }
 }
