6521d7e884d65264995511ac096467a85d59a6a5
marieono <104805318+marieono@users.noreply.github.com>
2022-07-31 07:34:03 +0900

    直った？

---
+++ src/components/modal.js
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+const modalRoot = document.getElementById("modal-root");
+
+const Container = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    widht: 100%;
+    background-color: rgba(0,0,0, .5);
+`
+
+export const Modal = () => {
+    return ReactDOM.createPortal(
+        <div>モーダル</div>,
+        modalRoot,
+    )
+}
