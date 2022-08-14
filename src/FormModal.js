801d9b46b63e4b628838b2db465231debfbc4fb9
marieono <104805318+marieono@users.noreply.github.com>
2022-08-14 10:20:50 +0900

    テーマ変更完成

--- src/FormModal.js
+++ src/FormModal.js
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import styled from "styled-components";
 import { Button } from "./components/button";
 import { Modal } from "./components/modal";
+import { ThemeContext, THEMES } from "./contexts/ThemeContext";
 
 const Container = styled.div`
     width: 240px;
     border-radius:10px;
     padding:24px 36px;
     background-color: white;
+    color:${({ theme }) => theme.color};
+    background-color: ${({ theme }) => theme.backgroundColor};
+    border: ${({ theme }) => theme === THEMES.dark ? '2px solid white' : 'none'};
 `
 
 const ButtonWrapper = styled.div`
@@ -15,9 +20,10 @@ const ButtonWrapper = styled.div`
     margin-top: 24px;
 `
 export const FormModal = ({ confirm, cancel }) => {
+    const [theme] = useContext(ThemeContext)
     return (
         <Modal>
-            <Container>
+            <Container theme={theme}>
                 <div>本当に作成しますか？</div>
                 <ButtonWrapper>
                     <Button onClick={cancel}>Cancel</Button>
